93d3a33eca0fccea3b862ed3763939a662a08be7
kjkandrea <kjkandrea@gmail.com>
2022-11-30 12:58:09 +0900

    feat: upload. read filenames

--- src/upload.ts
+++ src/upload.ts
@@ -1,1 +1,12 @@
+const fs = require('fs');
+
 console.log('upload!');
+
+function readLocales(path: string) {
+  const JSONFilePaths = fs
+    .readdirSync(path)
+    .map((filename: string) => path + '/' + filename);
+  console.log(JSONFilePaths);
+}
+
+readLocales('./locales');
